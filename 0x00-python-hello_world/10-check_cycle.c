707ef9e588b87603805b9f13c690abc177386c86
HinnMachache <bennyhinn253.bh@gmail.com>
2024-01-02 03:28:48 -0800

    HELLO PYTHON

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/** 
+ * check_cycle - Checks if cycles exists in a list
+ * @list: Head pointer to the list
+ * Return: 0 if no Cycle, else 1
+ * Used Floyd's Tortoise and Hare
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	slow = list;
+	fast = list->next;
+	while (fast != NULL && fast->next != NULL && slow != NULL)
+	{
+		if (fast == slow)
+			return (0);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (1);
+}
