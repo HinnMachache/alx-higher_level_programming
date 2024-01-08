058a83219522f1eedccea593d55e214f796a38b3
HinnMachache <bennyhinn253.bh@gmail.com>
2024-01-08 10:51:08 -0800

    Data Structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for index in range(0, len(my_list), -1):
         print("{:d}".format(my_list[index]))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
         first_character = None
