3e53ef2d291999d77ede38666df6cf4e38211019
HinnMachache <bennyhinn253.bh@gmail.com>
2024-01-31 14:20:15 -0800

    OOP in Python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 """Create an Empty Square Class"""
+
+
 class Square:
     """Empty Class Square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,8 @@ Private instance attribute: size
 Instantiation with size (no type/value verification)
 You are not allowed to import any module
 """
+
+
 class Square:
     """ A Square that defines a square by: (based on 0-square.py)"""
     def __init__(self, size):
