8cb5150ca4405ef9b928e134c94a990569dfe986
HinnMachache <bennyhinn253.bh@gmail.com>
2024-02-09 02:23:58 -0800

    Inheritance in Python

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 # 9-rectangle.py
+
 """Define a Geometry class that inherits BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
+
 class Rectangle(BaseGeometry):
     """Child Class inhertting from BaseClass Geometry"""
+
     def __init__(self, width, height):
         """Constructs a new class
         Arguments:
