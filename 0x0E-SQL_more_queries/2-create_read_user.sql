cc571005e1b7ddcf35480fe937ac22a94273878d
HinnMachache <bennyhinn253.bh@gmail.com>
2024-03-13 11:43:51 -0700

    SQL more Queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 -- SET SELECT PRIVILEGE TO user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED AS 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'uder_0d_2'@'localhost' = 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates the table unique_id on your MySQL server.
 CREATE IF NOT EXISTS (
 	id INT DEFAULT 1 UNIQUE,
-	NAME VARCHAR(256));
+	name VARCHAR(256));
